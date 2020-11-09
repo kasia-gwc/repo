57b73e292d31e8fe6e28fe95485284358174e995
Ahmad Rami <ahmadramen@gmail.com>
2020-11-09 09:11:03 +0000

    pseudo code for dynamic form

--- app.js
+++ app.js
@@ -9,6 +9,8 @@
  *  .then(response => console.log(response)) response will consist { ok, result }
  *  .catch(error => console.log(error))
  */
+createCard('asd')
+
 const mainContainer = document.querySelector('.main-container')
 const longLink = document.getElementById('long-link-form')
 
@@ -18,14 +20,9 @@ longLink.addEventListener('submit', (event) => {
 
   const url = event.target.link.value
   const apiUrl = `https://api.shrtco.de/v2/shorten?url=${url}`
-
   fetch(apiUrl)
     .then((response) => response.json())
     .then((nic) => {
-      const shortUrl = nic.result.short_link
-      mainContainer.dispatchEvent(
-        new CustomEvent('notify', { detail: shortUrl })
-      )
     })
     .catch((error) => console.log(error))
 })
@@ -33,3 +30,39 @@ longLink.addEventListener('submit', (event) => {
 mainContainer.addEventListener('notify', (event) => {
   console.log('notified triggered', event)
 })
+
+/* 
+  <div class="card">
+    <form id="short-link-form">
+      <input type="text" placeholder="shrtco.de/S2RynK" />
+      <input type="submit" class="button-copy" value="Copy" />
+    </form>
+  </div> 
+*/
+
+
+/**
+ * 0. Create a function first and pass a param then execute it after line 25 
+ * 1. Create an empty div using document.createElement('div') and give and add a class using classList.add('card') const cardElement
+ * 2. Create a form in javascript using document.createElement('form') and give a id short-link-form e.g. const formElement
+ * 3. Create an input element in js using document.createElement('input') and assign it to a constant e.g const inputShortLink =
+ *  3.1 set value to what you get from the api result.short_link
+ *  3.2 set readonly to true
+ * 4 Create an input element using document.createElement('input') and assign it to a const buttonElement
+ *  4.1 set type submit
+ *  4.2 add a class using .classList.add('button-copy')
+ *  4.3 set value to Copy
+ * 5. Append number 3 and number 4 to number 2 using appendChild e.g. formElement.appendChild([inputShortLink, buttonElement])
+ * 6. Append formElement to cardElement.appendChild(formElement)
+ * 7. Append cardElement to shorten-links-container element
+ */
+
+/**
+ * stage 2
+ * 1. addEventListener 'click' to the buttonElement and enure preventDefault happens
+ * 2. When the button is clicked, toggle class button-copy with button-copied
+ * 3. to copy the short_link to the clipboard
+ *
+ */
+
+ 
